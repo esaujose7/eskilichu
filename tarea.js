a994280750845f41adf8ed5e7534de876339afb1
tomas <tomasregnerfilippini@gmail.com>
2025-04-23 11:58:21 -0300

    tarea-1

---
+++ tarea.js
@@ -0,0 +1,26 @@
+const palabraMasLarga = () => {
+  const frase = process.argv[2];
+    let palabra = '';
+    let palabraLarga = '';
+
+    if(frase==undefined){
+      return console.log("error, frase mal ingresada");
+    };
+  
+    for (let i = 0; i <= frase.length; i++) {
+      const letra = frase[i];
+  
+      if (letra !== ' ' && i !== frase.length) {
+        palabra += letra;
+      } else {
+        if (palabra.length >= palabraLarga.length) {
+          palabraLarga = palabra;
+        }
+        palabra = '';
+      }
+    }
+    console.log("la palabra mas larga es",palabraLarga,"con",palabraLarga.length,"caracteres");
+}; 
+
+palabraMasLarga();
+
