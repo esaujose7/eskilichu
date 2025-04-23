14f8891263408223cdf54d66b3b3294080baec2e
tomas <tomasregnerfilippini@gmail.com>
2025-04-23 12:18:51 -0300

    tarea-1

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 console.log('https://www.youtube.com/shorts/7WqvLhKXvJE')
 
-const palabrerio = require("./tarea");
 
-
-console.log(palabrerio);
 
--- tarea.js
+++ tarea.js
@@ -24,4 +24,3 @@ const palabraMasLarga = () => {
 
 palabraMasLarga();
 
-module.exports = palabraMasLarga;
